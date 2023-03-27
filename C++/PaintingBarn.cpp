e3421facf881f679b2fa1d93955b6447dba90232
Rishabh Goel <rishabhgoel@Rishabhs-Air.localdomain>
2023-03-27 09:54:52 -0700

    Slight changes

---
+++ C++/PaintingBarn.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int wall[1001][1001];
+
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+
+    while (n--){
+        int a, b, c, d;
+        
+    }
+}
+
